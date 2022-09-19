52dca2da1068e5e2de8958394dbcc13501b97682
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-09-19 19:50:36 +0300

    add configuration files and HW_1_1

---
+++ src/HW_1_1.java
@@ -0,0 +1,10 @@
+// https://docs.google.com/document/d/1MurmMbcARrlOG1l9T9PCZJgLgaFfG3bKk_GvQXXeEDs/edit
+
+public class HW_1_1 {
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Поехали!!!");
+    }
+}
