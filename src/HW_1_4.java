22b3a1377a008b384fab7ea6c5ee7104907cd07c
m-maksimenka <maryna-maksimenka@protonmail.com>
2022-09-20 21:24:08 +0300

    add HW_1_4

---
+++ src/HW_1_4.java
@@ -0,0 +1,7 @@
+// https://docs.google.com/document/d/1MurmMbcARrlOG1l9T9PCZJgLgaFfG3bKk_GvQXXeEDs/edit
+
+public class HW_1_4 {
+    public static void main(String[] args) {
+        System.out.println("\u263A");
+    }
+}
